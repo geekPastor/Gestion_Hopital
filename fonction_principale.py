7d4bac9a00fcf67c76169401b19d7b04e7e3116e
Chrinovic-MM <chrinovicmukeba123@gmail.com>
2022-05-20 14:36:18 +0200

    résolution des bugs dans le fichier docteur

--- fonction_principale.py
+++ fonction_principale.py
@@ -2,7 +2,7 @@
 import os
 from clear_consol import clear_consol
 from patients import enregistre_patient, chercher_patient_avec_numero_dossier, afficher_patient, afficher_les_plaintes_patient, afficher_imc_patient, chercher_patient_avec_ses_identifiants
-from docteur import enregistre_docteur,  afficher_docteur, ajouter_ou_changer_horaire,  afficher_horaire,  enregistrer_horaire
+from docteur import enregistre_docteur,  afficher_docteur, ajouter_horaire,  afficher_horaire,  enregistrer_horaire, changer_horaire
 print("################################################################")
 print("##                                                            ##")
 print("##         PROGRAMME DE GESTION D'UN HOPITAL EN PYTHON        ##")
@@ -20,7 +20,7 @@ def fonction_principale():
     print("##                       MENU PRINCIPALE                      ##")
     print("##                                                            ##")
     print("################################################################")
-    print(f'Voici les actions que vous pouvez réaliser dans ce programme\n0 :Pour quitter le programme\n1: Pour Enregistrer un docteur\n2: Pour Enregistrer un patient\n3: Chercher un patient par ses identités\n4: Chercher un patient par le numero de dossier\n5: Afficher tous les patients\n6: Afficher tous les docteurs\n7: Enregistrer l\'horaire de chaque medecin\n8: Afficher les plaintes d\'un patient à partir de son numero unique\n9: Afficher l\'IMC (indice de masse corporel) d\'un patient\n10: Pour Afficher l\'horaire d\'un médecin\n11 :Pour ajouter ou changer l\'horaire d\'un médecin')
+    print(f'Voici les actions que vous pouvez réaliser dans ce programme\n0: Pour quitter le programme\n1: Pour Enregistrer un docteur\n2: Pour Enregistrer un patient\n3: Chercher un patient par ses identités\n4: Chercher un patient par le numero de dossier\n5: Afficher tous les patients\n6: Afficher tous les docteurs\n7: Enregistrer l\'horaire de chaque medecin\n8: Afficher les plaintes d\'un patient à partir de son numero unique\n9: Afficher l\'IMC (indice de masse corporel) d\'un patient\n10: Pour Afficher l\'horaire d\'un médecin\n11: Pour ajouter à l\'horaire d\'un docteur\n12: Pour changer l\'horaire d\'un docteur')
     choix = input ("\n>>>>")
     if choix.lower() == "0":
         exit()
@@ -71,7 +71,7 @@ def fonction_principale():
         nom = input("Entrez le nom du docteur pour leque vous voulez Enregistrer l'horaire: ")
         postnom = input("Entrez le postnom du docteur pour leque vous voulez Enregistrer l'horaire: ")
         prenom = input("Entrez le prenom du docteur pour leque vous voulez Enregistrer l'horaire: ")
-        enregistrer_horaire(prenom, postnom, nom)
+        enregistrer_horaire(nom, postnom, prenom)
         os.system('pause')
         clear_consol()
         fonction_principale()
@@ -83,7 +83,8 @@ def fonction_principale():
         fonction_principale()
         
     elif choix.lower() == "9":
-        afficher_imc_patient()
+        numero_dossier = input("Entrez le numéro du dossier du patient: ")
+        afficher_imc_patient(numero_dossier)
         os.system('pause')
         clear_consol()
         fonction_principale()
@@ -98,10 +99,16 @@ def fonction_principale():
         fonction_principale()
         
     elif choix.lower() == "11":
-        ajouter_ou_changer_horaire()
+        jour = input("Pour quel jour vous voulez ajouter la tâche: ")
+        nomDocteur = input("Entrez le nom du docteur pour lequel vous voulez ajouter une tâche à l'horaire: ")
+        ajouter_horaire(jour, nomDocteur)
         os.system('pause')
         clear_consol()
         fonction_principale()
+    elif choix.lower() == "12":
+        jour = input("Pour quel jour vous voulez ajouter la tâche: ")
+        nomDocteur = input("Entrez le nom du docteur pour lequel vous voulez ajouter une tâche à l'horaire: ")
+        changer_horaire(jour, nomDocteur)
         
     else :
         print("Cette fonction n'est pas reconnu !\n\n")
